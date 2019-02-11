767c2d550a726e6dec81cacc0e789bc1eabd3daf
BKinya <gitongakinya1@gmail.com>
2019-02-11 16:48:50 +0300

    improved solution

--- min-max_sum.py
+++ min-max_sum.py
@@ -3,10 +3,19 @@ k = 4
 
 num_sum = sum(arr[i] for i in range(len(arr)))# sum of all integers
 result = sum(arr[i] for i in range(4))#sum of the first 4 elements
-sum_list = [] # list to store sum of all possible combinations of 4 integers
+
+max_sum = result
+min_sum = result
+
 
 for i in range(0, k):
-     current = result, num_sum - arr[i]
-     sum_list.extend(current)
-print(str(min(sum_list)) + " " +str(max(sum_list)) )
+     current =  num_sum - arr[i]# get sum of all possible combinations of 4 integers
+     #get the maximum sum
+     if current > max_sum:
+          max_sum = current
+     
+     #get the minimum result
+     if current < min_sum:
+          min_sum = current
+print( str(max_sum)+ " "+str(min_sum))
      
